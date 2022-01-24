f11281858124cae1cb0445524942c19ff48a1e41
leskoarpi <71691273+leskoarpi@users.noreply.github.com>
2022-01-24 10:04:42 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+'''
+3. Feladat
+Írj egy programot while ciklussal, amely kiírja a páratlan számokat csökkenő sorrendben 1 és 10 között!
+'''
+
+x = 11
+while x > 0:
+    if x % 2 == 0:
+        print(x)
+    x-=1
